eca1f8165f39268f099bbd123e924935ce8f1ebd
lamisaid-services <lamisaidservices247@gmail.com>
2026-06-03 18:20:06 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'solar-v1';
+const urlsToCache = [
+  './index.html', './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(k => k !== CACHE_NAME && caches.delete(k))
+    ))
+  );
+  self.clients.claim();
+});
